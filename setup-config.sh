bb1d271ba42204f8de1680246fd49b1687398b88
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2026-01-28 17:24:11 -0300

    Adjusting the Path of the Files

--- setup-config.sh
+++ setup-config.sh
@@ -7,10 +7,10 @@ if [ ! -d /etc/systemd/system ]; then
 fi
 
 # Copy service and timer files to systemd directory
-cp ./sys_watch.service /etc/systemd/system/sys_watch.service
-cp ./sys_watch.timer /etc/systemd/system/sys_watch.timer
+cp ./unit_files/sys_watch.service /etc/systemd/system/sys_watch.service
+cp ./unit_files/sys_watch.timer /etc/systemd/system/sys_watch.timer
 
 # Reload systemd to recognize new service and timer
 sudo systemctl daemon-reload
-echo "Service and timer have been set up successfully."
-echo "You can start the timer with: sudo systemctl start sys_watch.timer"
+echo "- Service and timer have been set up successfully."
+echo "- You can start the timer with: sudo systemctl start sys_watch.timer"
