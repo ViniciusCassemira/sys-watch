1b7bc8271641ac55a524b37c8fe692df630f9753
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2026-01-30 14:37:46 -0300

    ✏️ fix: using .env value to find log files

--- functions/write_log.py
+++ functions/write_log.py
@@ -1,9 +1,14 @@
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
 # Filter the date and current time
 current_time = datetime.now().strftime('%Y-%m-%d %X')
 
+load_dotenv()
+working_directory = os.getenv("WORKING_DIRECTORY")
+
 # Function to log messages in .txt files with the current timestamp
 def log_message(file_name,message):
-    with open(f"/home/vinicius/Dev/sys-watch/data/{file_name}.txt", "a") as file:
+    with open(f"{working_directory}/data/{file_name}.txt", "a") as file:
         file.write(f"{current_time} {message}\n")
